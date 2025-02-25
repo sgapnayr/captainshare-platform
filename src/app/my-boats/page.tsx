0bbd84d3454b2672d29a68333fdd263871a07535
sgapnayr <100978214+sgapnayr@users.noreply.github.com>
2025-02-24 23:24:43 -0600

    clean

--- src/app/my-boats/page.tsx
+++ src/app/my-boats/page.tsx
@@ -289,7 +289,6 @@ export default function MyBoatsPage() {
                                     )}
                                   </p>
                                 </div>
-                                {/* Add more maintenance details as needed */}
                               </div>
                             </DialogContent>
                           </Dialog>
